a15a8ea5279168ac5987917d06e920aab05d777e
DanielZhu <enterzhu@gmail.com>
2016-05-20 05:08:20 +0800

    Support to analyze data for one specified page & export markdown table in cli mode Support colorful cli

--- tools.js
+++ tools.js
@@ -6,6 +6,8 @@
 var fse = require('fs-extra');
 var path = require('path');
 var etpl = require('etpl');
+var chalk = require('chalk');
+var mkTable = require('markdown-table');
 
 etpl.config({
     commandOpen: '{{',
@@ -14,6 +16,64 @@ etpl.config({
 
 var writeToFile = function (filename, line) {
     fse.outputFileSync(filename, line, {encoding: 'utf8'});
+};
+
+/**
+ * Get the length of a string, minus ANSI color
+ * characters.
+ *
+ * @param {string} value The String
+ * @return {string} Length
+ */
+function stringLength(value) {
+    var length = chalk.stripColor(value).length;
+    return length;
+}
+
+var printMarkdownTable = function (title, tableData) {
+    var now = new Date().getTime();
+    var data = [];
+    var initedHeader = false;
+    var tableHeader = [''];
+    var tableRowAlign = ['l'];
+    for (var indexItem in tableData) {
+        if (tableData.hasOwnProperty(indexItem)) {
+            var tableBodyRow = [];
+            tableBodyRow.push(indexItem);
+            for (var subIndexItem in tableData[indexItem]) {
+                if (tableData[indexItem].hasOwnProperty(subIndexItem)) {
+                    if (!initedHeader) {
+                        tableHeader.push(subIndexItem);
+                        tableRowAlign.push('c');
+                    }
+                    tableBodyRow.push(tableData[indexItem][subIndexItem]);
+                }
+            }
+            if (!initedHeader) {
+                initedHeader = true;
+                data.push(tableHeader);
+            }
+            // console.log('table row: ' + JSON.stringify(tableBodyRow));
+            data.push(tableBodyRow);
+        }
+    }
+    console.log(this.timeDiff(now) + 'preparing the table data...');
+    // console.log('table data: ' + JSON.stringify(data));
+    console.log('');
+    console.log(chalk.white('------- Performance Testing Results -------'));
+    console.log('');
+    console.log(chalk.yellow('Page : ' + title));
+    console.log('');
+
+    now = new Date().getTime();
+    console.log(chalk.white(mkTable(data, {
+        align: tableRowAlign,
+        stringLength: stringLength
+    })));
+
+    console.log('');
+    console.log('');
+    console.log(this.timeDiff(now) + 'finish render data');
 };
 
 var renderHtml = function (pageList, perfData) {
@@ -27,4 +87,9 @@ var renderHtml = function (pageList, perfData) {
     writeToFile('report.' + new Date().getTime() + '.html', compiled);
 };
 
+var timeDiff = function (previous) {
+    return chalk.yellow(new Date().getTime() - previous + 'ms - ');
+}
+module.exports.timeDiff = timeDiff;
+module.exports.printMarkdownTable = printMarkdownTable;
 module.exports.renderHtml = renderHtml;
